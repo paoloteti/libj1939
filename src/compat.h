96e299476ae3bd139a832a110a1a34c25785fbd2
Paolo Teti <paolo.teti@gmail.com>
2020-04-21 15:09:23 +0200

    compat.h: add support for WIN32/64 platforms
    Add support for WIN32/64 under MSVC and gcc/clang
    
    Signed-off-by: Paolo Teti <paolo.teti@gmail.com>

--- src/compat.h
+++ src/compat.h
@@ -3,14 +3,24 @@
 #define __COMPAT_H__
 
 #if defined(__linux__)
-#include <endian.h>
+#   include <endian.h>
+#elif defined(_WIN32) || defined(_WIN64)
+#   include <windows.h>
+#   if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#       if defined(_MSC_VER)
+#           include <stdlib.h>
+#		    define htobe64(x) _byteswap_uint64(x)
+#       elif defined(__GNUC__) || defined(__clang__)
+#			define htobe64(x) __builtin_bswap64(x)
+#       endif
+#   endif
 #else
-#include <machine/endian.h>
-#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-#define htobe64 __bswap64
-#else
-#define htobe64
-#endif /* __BYTE_ORDER__ */
+#   include <machine/endian.h>
+#   if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#       define htobe64 __bswap64
+#   else
+#       define htobe64
+#   endif
 #endif
 
 #endif /* __COMPAT_H__ */
