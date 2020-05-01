4e7c99ed22b55446f73ba5256e176325cf8aa874
Paolo Teti <paolo.teti@gmail.com>
2020-05-01 13:55:54 +0200

    Fix detection of atomic builtins
    Use HAVE_STDATOMIC_H as provided by cmake via config.h
    
    Signed-off-by: Paolo Teti <paolo.teti@gmail.com>

--- src/atomic.h
+++ src/atomic.h
@@ -13,9 +13,7 @@ typedef atomic_t atomic_val_t;
 #define ATOMIC_MASK(bit) (1 << ((uint32_t)(bit) & (ATOMIC_BITS - 1)))
 #define ATOMIC_ELEM(addr, bit) ((addr) + ((bit) / ATOMIC_BITS))
 
-#define HAVE_ATOMIC_BUILTINS
-
-#ifdef HAVE_ATOMIC_BUILTINS
+#ifdef HAVE_STDATOMIC_H
 static inline atomic_val_t atomic_get(const atomic_t *target)
 {
 	return __atomic_load_n(target, __ATOMIC_SEQ_CST);
@@ -58,7 +56,7 @@ extern atomic_val_t atomic_get(const atomic_t *target);
 extern atomic_val_t atomic_or(atomic_t *target, atomic_val_t value);
 extern atomic_val_t atomic_and(atomic_t *target, atomic_val_t value);
 extern void atomic_set(atomic_t *target, atomic_val_t x);
-exter bool atomic_test_and_set_bit(atomic_t *target, int bit);
+extern bool atomic_test_and_set_bit(atomic_t *target, int bit);
 extern void atomic_clear_bit(atomic_t *target, int bit);
 #endif
 
