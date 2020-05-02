3d07df9e480988019dba034305964ae283e8e245
Paolo Teti <paolo.teti@gmail.com>
2020-05-02 16:41:46 +0200

    atomic: remove atomic_val_t
    Just use atomic_t for input and output values
    
    Signed-off-by: Paolo Teti <paolo.teti@gmail.com>

--- src/atomic.h
+++ src/atomic.h
@@ -5,39 +5,38 @@
 #include "config.h"
 
 typedef int atomic_t;
-typedef atomic_t atomic_val_t;
 
 #define ATOMIC_INIT(_v) (_v)
 
-#define ATOMIC_BITS (sizeof(atomic_val_t) * 8)
+#define ATOMIC_BITS (sizeof(atomic_t) * 8)
 #define ATOMIC_MASK(bit) (1 << ((uint32_t)(bit) & (ATOMIC_BITS - 1)))
 #define ATOMIC_ELEM(addr, bit) ((addr) + ((bit) / ATOMIC_BITS))
 
 #ifdef HAVE_STDATOMIC_H
-static inline atomic_val_t atomic_get(const atomic_t *target)
+static inline atomic_t atomic_get(const atomic_t *target)
 {
 	return __atomic_load_n(target, __ATOMIC_SEQ_CST);
 }
 
-static inline atomic_val_t atomic_or(atomic_t *target, atomic_val_t value)
+static inline atomic_t atomic_or(atomic_t *target, atomic_t value)
 {
 	return __atomic_fetch_or(target, value, __ATOMIC_SEQ_CST);
 }
 
-static inline atomic_val_t atomic_and(atomic_t *target, atomic_val_t value)
+static inline atomic_t atomic_and(atomic_t *target, atomic_t value)
 {
 	return __atomic_fetch_and(target, value, __ATOMIC_SEQ_CST);
 }
 
-static inline void atomic_set(atomic_t *target, atomic_val_t x)
+static inline void atomic_set(atomic_t *target, atomic_t x)
 {
 	__atomic_store_n(target, x, __ATOMIC_SEQ_CST);
 }
 
 static inline bool atomic_test_and_set_bit(atomic_t *target, int bit)
 {
-	atomic_val_t mask = ATOMIC_MASK(bit);
-	atomic_val_t old;
+	atomic_t mask = ATOMIC_MASK(bit);
+	atomic_t old;
 
 	old = atomic_or(ATOMIC_ELEM(target, bit), mask);
 
@@ -46,16 +45,16 @@ static inline bool atomic_test_and_set_bit(atomic_t *target, int bit)
 
 static inline void atomic_clear_bit(atomic_t *target, int bit)
 {
-	atomic_val_t mask = ATOMIC_MASK(bit);
+	atomic_t mask = ATOMIC_MASK(bit);
 
 	(void)atomic_and(ATOMIC_ELEM(target, bit), ~mask);
 }
 
 #else
-extern atomic_val_t atomic_get(const atomic_t *target);
-extern atomic_val_t atomic_or(atomic_t *target, atomic_val_t value);
-extern atomic_val_t atomic_and(atomic_t *target, atomic_val_t value);
-extern void atomic_set(atomic_t *target, atomic_val_t x);
+extern atomic_t atomic_get(const atomic_t *target);
+extern atomic_t atomic_or(atomic_t *target, atomic_t value);
+extern atomic_t atomic_and(atomic_t *target, atomic_t value);
+extern void atomic_set(atomic_t *target, atomic_t x);
 extern bool atomic_test_and_set_bit(atomic_t *target, int bit);
 extern void atomic_clear_bit(atomic_t *target, int bit);
 #endif
