e63b7ae39455be964ed6524f066032e940ad0379
Paolo Teti <paolo.teti@gmail.com>
2020-05-23 16:19:43 +0200

    config-pcan0.sh: shutdown interface
    shutdown interface before bit-rate setup.
    
    Signed-off-by: Paolo Teti <paolo.teti@gmail.com>

--- example/config-pcan0.sh
+++ example/config-pcan0.sh
@@ -2,8 +2,10 @@
 
 # configure PEAK PCAN-USB Pro
 
+ip link set can0 down
 ip link set can0 type can bitrate 500000
 ip link set can0 up
 
+ip link set can1 down
 ip link set can1 type can bitrate 500000
 ip link set can1 up
